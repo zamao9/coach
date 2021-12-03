3047f77d799eb8851466ecacfeb844e53490b3a1
zamao9 <94758143+zamao9@users.noreply.github.com>
2021-12-03 18:24:13 +0300

    asf

--- js/coach.js
+++ js/coach.js
@@ -1,17 +1,26 @@
 $(function() {
 
+           /* БУРГЕР */
+
   $('.burger').click(function(event) {
     event.preventDefault();
 
     $(this).toggleClass('active');
     $('.nav').toggleClass('active');
     $('.header').toggleClass('active');
+
+
   });
+
+            /* АКТИВНОСТЬ ССЫЛОК НАВИГАЦИИ */
 
   $('.links').click(function() {
     $('.links').removeClass('active');
     $(this).toggleClass('active');
-  })
+  });
+
+
+
 
 
 
