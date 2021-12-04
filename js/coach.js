af99bf29f1ecd0edd86f58ca82bcbd99f6085bc7
zamao9 <94758143+zamao9@users.noreply.github.com>
2021-12-05 02:55:26 +0300

    asfsd

--- js/coach.js
+++ js/coach.js
@@ -26,6 +26,24 @@ $(function() {
       $('.header').removeClass('active');
   });
 
+  AOS.init({
+    disable: false,
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+    offset: 0,
+    delay: 0,
+    duration: 1500,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-top',
+  })
 
 
 
