3cdd237466fa35b77770f9b6e1c5632c72fe8d1d
zamao9 <94758143+zamao9@users.noreply.github.com>
2021-12-22 17:36:05 +0300

    gsd

--- js/coach.js
+++ js/coach.js
@@ -1,53 +1,93 @@
-$(function() {
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows());
+  }
+};
 
-           /* БУРГЕР */
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+} else {
+  document.body.classList.add('_pc');
+};
+
+
+
+
+$(function () {
+
+
+                              /* SCROLL */
+
+  $("[data-scroll]").click(function(event) {
+    event.preventDefault();
+
+    var blockId = $(this).data('scroll'),
+        blockOffset = $(blockId).offset().top - document.querySelector('header').offsetHeight;
+
+    $("html, body").animate ({
+      scrollTop: blockOffset
+    }, 1000);
+  });
+
+  var headerH = $("#header").innerHeight(),
+      scrollOffset = 0;
+
+
+  $(window).on("scroll", function() {
+
+    scrollOffset = $(this).scrollTop();
+
+    if( scrollOffset >= 300) {
+      $('.scroll').addClass('fixed');
+    } else {
+      $('.scroll').removeClass('fixed');
+    }
+
+  });
+
+
+
+
+                              /* BURGER */
 
   $('.burger').click(function(event) {
     event.preventDefault();
 
     $(this).toggleClass('active');
-    $('.nav').toggleClass('active')
-    $('.header').toggleClass('active');
 
   });
 
-            /* АКТИВНОСТЬ ССЫЛОК НАВИГАЦИИ */
 
-  $('.links').click(function() {
-    $('.links').removeClass('active');
-    $(this).toggleClass('active');
+
+
+                              /* SLIDER */
+
+  $('.slider').slick({
+    fade: true,
+    speed: 1000,
+    autoplay: true,
+    auyoplaySpeed: 2000,
   });
-
-  $('.links').click(function(event) {
-      event.preventDefault();
-
-      $('.nav').removeClass('active');
-      $('.burger').removeClass('active');
-      $('.header').removeClass('active');
-  });
-
-  AOS.init({
-    disable: false,
-    startEvent: 'DOMContentLoaded',
-    initClassName: 'aos-init',
-    animatedClassName: 'aos-animate',
-    useClassNames: false,
-    disableMutationObserver: false,
-    debounceDelay: 50,
-    throttleDelay: 99,
-
-    offset: 0,
-    delay: 0,
-    duration: 3000,
-    easing: 'ease',
-    once: true,
-    mirror: false,
-    anchorPlacement: 'top-bottom',
-  })
-
-
-
-
 
 
 
