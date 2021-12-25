1c2bfd8a60e7eb1fa6d0608051fb8a29a60edee7
zamao9 <94758143+zamao9@users.noreply.github.com>
2021-12-25 09:34:53 +0300

    wafs

---
+++ about/js/about.js
@@ -0,0 +1,106 @@
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows());
+  }
+};
+
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+} else {
+  document.body.classList.add('_pc');
+};
+
+
+
+
+$(function () {
+
+
+                              /* SCROLL */
+
+  $("[data-scroll]").click(function(event) {
+    event.preventDefault();
+
+    var blockId = $(this).data('scroll'),
+        blockOffset = $(blockId).offset().top - document.querySelector('header').offsetHeight;
+
+    $("html, body").animate ({
+      scrollTop: blockOffset
+    }, 1000);
+  });
+
+  var headerH = $("#about").innerHeight(),
+      scrollOffset = 0;
+
+
+  $(window).on("scroll", function() {
+
+    scrollOffset = $(this).scrollTop();
+
+    if( scrollOffset >= 10) {
+      $('.scroll').addClass('fixed');
+    } else {
+      $('.scroll').removeClass('fixed');
+    }
+
+  });
+
+
+
+
+                              /* BURGER */
+
+  $('.burger').click(function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass('active');
+    $('.nav').toggleClass('active');
+    $('.header').toggleClass('active');
+
+  });
+
+  $('.links').click(function(event) {
+    event.preventDefault();
+
+    $("#nav a").removeClass('active');
+    $(this).addClass('active');
+
+    $('.header').removeClass('active');
+    $('.nav').removeClass('active');
+    $('.burger').removeClass('active');
+
+  });
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
